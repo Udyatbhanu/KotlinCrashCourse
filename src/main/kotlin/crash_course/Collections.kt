6986d7f40de58b556c171acf8e855a4ecc0a9ceb
Bhanu Baliyar <quizzwhizz1467@gmail.com>
2023-02-22 12:33:56 -0800

    Set

--- src/main/kotlin/crash_course/Collections.kt
+++ src/main/kotlin/crash_course/Collections.kt
@@ -6,17 +6,22 @@ fun main(){
 //    fruits[0] = "Bananas"
 //    println(fruits[0])
 
-    val fruitList = listOf("Apples", "Oranges", "Pears", "Grapes")
-    val fruitListVal = mutableListOf("Apples", "Oranges", "Pears", "Grapes")
+//    val fruitList = listOf("Apples", "Oranges", "Pears", "Grapes")
+//    val fruitListVal = mutableListOf("Apples", "Oranges", "Pears", "Grapes")
+//
+//    var fruitListImmutableVar = listOf("Apples", "Oranges", "Pears", "Grapes")
+//    var fruitListMutableVar = mutableListOf("Apples", "Oranges", "Pears", "Grapes")
+//
+//    var fruitListMutableVarArrayList = arrayListOf("Apples", "Oranges", "Pears", "Grapes")
+//
+//    fruitListMutableVarArrayList.add("Cherries")
+//
+//
+//
+//    println(fruitListMutableVarArrayList[1])
 
-    var fruitListImmutableVar = listOf("Apples", "Oranges", "Pears", "Grapes")
-    var fruitListMutableVar = mutableListOf("Apples", "Oranges", "Pears", "Grapes")
+    val fruitSet = mutableSetOf("Apples", "Oranges", "Pears", "Grapes")
+    fruitSet.add("apples")
 
-    var fruitListMutableVarArrayList = arrayListOf("Apples", "Oranges", "Pears", "Grapes")
-
-    fruitListMutableVarArrayList.add("Cherries")
-
-
-
-    println(fruitListMutableVarArrayList[1])
+    println( fruitSet.first())
 }
