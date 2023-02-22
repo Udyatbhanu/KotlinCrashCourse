d2a18a271c79c64e580ad2fe66cb274e4a87b41b
Bhanu Baliyar <quizzwhizz1467@gmail.com>
2023-02-21 17:31:06 -0800

    Collections List

--- src/main/kotlin/crash_course/Collections.kt
+++ src/main/kotlin/crash_course/Collections.kt
@@ -1,8 +1,22 @@
 package crash_course
 
 fun main(){
-    val fruits = arrayOf("Apples", "Oranges", "Pears", "Grapes")
-    println(fruits[0])
-    fruits[0] = "Bananas"
-    println(fruits[0])
+//    val fruits = arrayOf("Apples", "Oranges", "Pears", "Grapes")
+//    println(fruits[0])
+//    fruits[0] = "Bananas"
+//    println(fruits[0])
+
+    val fruitList = listOf("Apples", "Oranges", "Pears", "Grapes")
+    val fruitListVal = mutableListOf("Apples", "Oranges", "Pears", "Grapes")
+
+    var fruitListImmutableVar = listOf("Apples", "Oranges", "Pears", "Grapes")
+    var fruitListMutableVar = mutableListOf("Apples", "Oranges", "Pears", "Grapes")
+
+    fruitListImmutableVar = emptyList()
+    fruitListMutableVar = emptyList<String>().toMutableList()
+    fruitListMutableVar.add("Cherries")
+
+
+
+    println(fruitListMutableVar.size)
 }
