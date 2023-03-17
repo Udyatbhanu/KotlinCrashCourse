76d524f049c8aa63acc3bec4adf376c00a40ca98
Bhanu Baliyar <quizzwhizz1467@gmail.com>
2023-03-17 13:14:11 -0700

    changes

--- src/main/kotlin/crash_course/Collections.kt
+++ src/main/kotlin/crash_course/Collections.kt
@@ -13,5 +13,41 @@ fun main(){
         Pair("Sara" ,23),
         Pair("Drake" ,30),
     )
-    println(ageMap["Jack"])
+//    println(ageMap["Jack"])
+
+    val fruitsList = listOf("Apples", "Oranges", "Peaches", "Apricot")
+    val fruitSet = setOf("Apples", "Oranges", "Peaches", "Apricot")
+
+    val fruit = fruitSet.find {
+        it == "Peaches"
+    }
+
+    println(fruit)
+//    fruitSet.iterator()
+//     for(fruit in fruitSet){
+//
+//     }
+//    fruitsList.add("Banana")
+
+//    val startsWithA = fruitsList.filter {
+//        it.startsWith('A')
+//    }
+
+//    fruitsList.filter {
+//        it.endsWith('s', true)
+//    }.forEach {
+//        println(it)
+//    }
+
+//    val result = fruitsList.map{
+//        it.lowercase()
+//    }
+//
+//    fruitsList.filter { fruit ->
+//        fruit.startsWith('A')
+//
+//    }.forEach{
+//        println(it)
+//    }
+
 }
---
+++ src/main/kotlin/crash_course/Loops.kt
@@ -0,0 +1,14 @@
+package crash_course
+
+fun main() {
+
+    var isTrue = false
+
+    while(isTrue){
+        println("this is the while loop $isTrue")
+    }
+
+    do{
+        println("this is the DO while loop $isTrue")
+    }  while(isTrue)
+}
