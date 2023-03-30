708740191d31ca3af57ceddee33fa58aabe80f2a
Bhanu Baliyar <quizzwhizz1467@gmail.com>
2023-03-29 18:13:05 -0700

    getter and setter

--- src/main/kotlin/KotlinClasses.kt
+++ src/main/kotlin/KotlinClasses.kt
@@ -1,18 +1,24 @@
-class Person(age : Int) {
+class Person() {
 
-    init{
-        println("This is the init block")
-    }
+    var greeting: String = "Hello"
+        get() = field.plus(" ").plus("Kotlin")
+        set(value) {
+            field = value.plus(" ").plus("World")
+        }
 
-
-
-
+    var age: Int = -1
+//        get() = field
+//        set(value) {
+//            field = value
+//        }
 
 
 }
 
-fun main(){
-    val person1 = Person(20)
-    val person2 = Person(20)
+fun main() {
+    val person = Person()
+    person.greeting = "Hello"
+    println(person.greeting)
+
 
 }
